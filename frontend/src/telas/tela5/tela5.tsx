ddf723c807b420df77934d628d394950fbdfcf50
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-10 01:09:51 -0300

    site

--- frontend/src/telas/tela5/tela5.tsx
+++ frontend/src/telas/tela5/tela5.tsx
@@ -3,8 +3,7 @@ import './tela5.css'
 function Tela5() {
     return (
       <>
-        <h1> Onde estamos </h1>
-        <iframe className='GPS' src="https://www.google.com/maps/embed?pb=!4v1667603475683!6m8!1m7!1seF2VJKDJEcyLecg1e4Z2jQ!2m2!1d-22.32747282011934!2d-49.05342139433192!3f146.84192587426898!4f-1.7025899489030962!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        
       </>
     )
   }
