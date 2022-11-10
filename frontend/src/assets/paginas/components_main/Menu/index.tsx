067496381f1ab4ab4adca2111316ba7864062f44
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-10 17:14:24 -0300

    site

--- frontend/src/assets/paginas/components_main/Menu/index.tsx
+++ frontend/src/assets/paginas/components_main/Menu/index.tsx
@@ -3,11 +3,11 @@ import './styles.css'
 function Menu() {
     return(
         <nav className='nav nav-color'>
-            <a href='https://projetoreger.netlify.app/o-que-fazemos' className='imgfund_1 color1'>O Que Fazemos</a>
-            <a href='/como-fazemos' className='imgfund_2 color2'>Como Fazemos</a>
-            <a href='/crescemos' className='imgfund_3 color3'>Crescemos</a>
-            <a href='/onde-estamos' className='imgfund_4 color4'>Onde Estamos</a>
-            <a href='/dicas' className='imgfund_5 color5'>Dicas</a>
+            <a href='o-que-fazemos' className='imgfund_1 color1'>O Que Fazemos</a>
+            <a href='como-fazemos' className='imgfund_2 color2'>Como Fazemos</a>
+            <a href='crescemos' className='imgfund_3 color3'>Crescemos</a>
+            <a href='onde-estamos' className='imgfund_4 color4'>Onde Estamos</a>
+            <a href='dicas' className='imgfund_5 color5'>Dicas</a>
         </nav>
     )
 }
