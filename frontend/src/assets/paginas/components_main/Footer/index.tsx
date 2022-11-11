5dd6250d1326d0a822c9a8bc70c49ca596cf98a4
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-11 19:10:34 -0300

    site

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
         <footer className='footer1'>
             <div>
-            <a href='http://127.0.0.1:5173/creditos'> Desenvolvido por </a>
+            <a href='/creditos'> Desenvolvido por </a>
              <img className='face' width="30px" height="30px"/>
             </div>
          
